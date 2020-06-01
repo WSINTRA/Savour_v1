6bc07063c3b3bb7f3dfd96d0afc9db413ecd626e
WS <ws@WSs-MacBook-Pro.local>
2020-05-31 21:17:35 -0400

    Created namespace variables within App

--- App.js
+++ App.js
@@ -6,58 +6,74 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
-import Welcome from './components/Welcome.js';
-import LoginForm from './components/LoginForm.js'
+import Welcome from "./components/Welcome.js";
+import LoginForm from "./components/LoginForm.js";
 import { NavigationContainer } from "@react-navigation/native";
 import { View, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import gql from "graphql-tag";
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
 const Stack = createStackNavigator();
-const httpLink = createHttpLink({
-  uri: "http://192.168.0.12:4000",
-});
 
-//Using AsyncStorage for JWT tokens
-const getData = async (AUTH_TOKEN) => {
-  try {
-    const value = await AsyncStorage.getItem(AUTH_TOKEN);
-    if (value !== null) {
-      // We have data!!
-      return value;
+//Apollo created as a nameSpace for all functionality of Apollo
+const Apollo = {
+
+  httpLink: createHttpLink({
+    uri: "http://192.168.0.12:4000",
+  }),
+
+  getData: async (AUTH_TOKEN) => {
+    try {
+      const value = await AsyncStorage.getItem(AUTH_TOKEN);
+      if (value !== null) {
+        // We have data!!
+        return value;
+      }
+    } catch (error) {
+      // Error retrieving data
     }
-  } catch (error) {
-    // Error retrieving data
-  }
+  },
+
+  authLink : setContext((_, { headers }) => {
+    const token = Apollo.getData(AUTH_TOKEN).then((token) => {
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : ``,
+        },
+      };
+    });
+  }),
+
+  client : new ApolloClient({
+    link: Apollo.authLink.concat(Apollo.httpLink),
+    cache: new InMemoryCache(),
+  })
 };
-const SIGNUP_MUTATION = gql`
-  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
-    signup(email: $email, password: $password, name: $name) {
-      token
-    }
-  }
-`;
-const authLink = setContext((_, { headers }) => {
-  const token = getData(AUTH_TOKEN).then(token=>{
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ``,
-    },
-  }
-});
-});
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+
+
+//Saving this for register form page
+// const SIGNUP_MUTATION = gql`
+//   mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+//     signup(email: $email, password: $password, name: $name) {
+//       token
+//     }
+//   }
+// `;
 
 
 class App extends React.Component {
-  componentDidMount(){
-    token = getData(AUTH_TOKEN).then(data=>console.log("value is", data))
+  componentDidMount() {
+    try {
+      token = Apollo.getData(AUTH_TOKEN).then((data) =>
+        console.log("value is", data)
+      );
+    } catch {
+      console.log("no token found");
+    }
     //ToDO - Work on Refresh and access tokens for keeping user logged in...
+    //https://www.richardkotze.com/coding/send-jwt-client-apollo-graphql <- Looks worth a read
   }
   state = {
     buttonStyle: { backgroundColor: "#faf6f2", color: "#b5b5b5" },
@@ -67,30 +83,14 @@ class App extends React.Component {
     password: "",
     name: "",
     success: false,
-    username: ""
+    username: "",
     // succesful: {}
   };
 
-  _confirm = async (data) => {
-    let token = data.login.token
-    // const { token } = this.state.login ? data.login : data.signup
-    this._saveUserData(token, data);
-  };
-  _saveUserData = async (token, data) => {
-    try {
-      await AsyncStorage.setItem(AUTH_TOKEN, token)
-    } catch (e) {
-      console.log(e)
-    }
-    this.setState({
-      success: true,
-      username: data.login.user.name
-    })
-  };
+ //Keep all LoginForm functioncality inside the LoginFormFunctions namespace
+  LoginFormFunctions = {
   //Change the style color and form control
-  //passes down to login form for input values
-  changeTextInput = (property, value) => {
-    // console.log(property, value, this.state, "Log ")
+  changeTextInput : (property, value) => {
     this.setState({
       [property]: value,
     });
@@ -103,50 +103,62 @@ class App extends React.Component {
         buttonStyle: { backgroundColor: "#faf6f2", color: "#b5b5b5" },
       });
     }
-  };
-  //enables switching of the views between login form and signup
-  switchSignBool = (signup) => {
-    this.setState({ signup: !signup });
-  };
-  switchLoginBool = (login) => {
-    this.setState({ login: !login });
-  };
-
-  //when login is succesful, we get success, this controls view for user
-  loginSuccess = () => {
+  },
+  //Once data is returned from API, _confirm is fired
+  _confirm : async (data) => {
+    let token = data.login.token;
+    // const { token } = this.state.login ? data.login : data.signup
+    this.LoginFormFunctions._saveUserData(token, data);
+  },
+  _saveUserData : async (token, data) => {
+    try {
+      await AsyncStorage.setItem(AUTH_TOKEN, token);
+    } catch (e) {
+      console.log(e);
+    }
     this.setState({
       success: true,
+      username: data.login.user.name,
     });
-  };
+  }
+}
   render() {
-    const { signup, success, email, name, password, buttonStyle } = this.state;
+    const { success, email, password, buttonStyle } = this.state;
 
     return (
-      <ApolloProvider client={client}>
+      <ApolloProvider client={Apollo.client}>
         <NavigationContainer>
           <Stack.Navigator>
-            {success ? <Stack.Screen name="test" component={HomeScreen}/> : 
-            <>
-            <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}}/>
-            
-            <Stack.Screen name="LoginForm" options={{headerShown: false}}>
-              {props=> <LoginForm 
-              buttonStyle={buttonStyle} 
-              email={email}
-              changeInputText={this.changeTextInput}
-              password={password}
-              _confirm={this._confirm}
-              />}
-              
-            </Stack.Screen></>}
-            
+            {success ? (
+              <Stack.Screen name="test" component={HomeScreen} />
+            ) : (
+              <>
+                <Stack.Screen
+                  name="Welcome"
+                  component={Welcome}
+                  options={{ headerShown: false }}
+                />
+
+                <Stack.Screen name="LoginForm" options={{ headerShown: false }}>
+                  {(props) => (
+                    <LoginForm
+                      buttonStyle={buttonStyle}
+                      email={email}
+                      changeInputText={this.LoginFormFunctions.changeTextInput}
+                      password={password}
+                      _confirm={this.LoginFormFunctions._confirm}
+                    />
+                  )}
+                </Stack.Screen>
+              </>
+            )}
           </Stack.Navigator>
         </NavigationContainer>
       </ApolloProvider>
     );
   }
 }
-
+//Only exists for development stage
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
